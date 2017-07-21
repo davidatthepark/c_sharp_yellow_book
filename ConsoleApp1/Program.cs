42bc1bb540452b957f1166827c00f0ab798d3976
David Park <dpark4657@gmail.com>
2017-07-20 20:10:07 -0700

    initializes double glazer main method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class GlazerCalc
     {
+        // The static keyword makes sure that the method which follows is always present.
+        // You choose the names of your methods to reflect what they are going to do for you, except Main.
         static void Main(string[] args)
         {
+            // C# can handle three data types: floating point numbers, integer numbers, and text.
+            // A double is a double precision floating point number.
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+
+            Console.WriteLine("Welcome to Glazer Calculator. Please enter the width of the window.");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.WriteLine("Please enter the height of the window");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.2;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
         }
     }
 }
