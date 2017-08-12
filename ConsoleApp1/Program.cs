b280c3f1b7000d1ca027de614296f1bfe1778bc4
davidatthepark <dpark4657@gmail.com>
2017-08-12 13:18:00 -0700

    up to section 2.3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,14 +13,12 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // C# can handle three data types: floating point numbers, integer numbers, and text.
-            // A double is a double precision floating point number. Floats are simply numbers with decimal floats. From smallest to largest precision, we have floats(7), doubles(15), then decimal floats(29). Decimals use more space but they provide ultimate accuracy which is useful for financials. 
+            // A double is a double precision floating point number. Floats are simply numbers with decimal floats. From smallest to largest precision, we have floats(7), doubles(15), then decimal floats(29). Decimals use more space but they provide ultimate accuracy which is useful for things like currency. 
             // Most programmers tend to use int and float variable types to make the programs easier to understand. Better not to fuss about the wasted memory. In ACOM, we deal with numbers that require accuracy, hence the use of decimals. 
             // Choosing the right type of variable is a learned skill. Try to use floating point storage as a last resort. It lacks precision. Instead, you can represent 1.5 dollars as 150 cents.
 
-
             double width, height, woodLength, glassArea;
             string widthString, heightString;
-
 
             Console.WriteLine("Welcome to Glazer Calculator. Please enter the width of the window.");
             widthString = Console.ReadLine();
@@ -31,7 +29,7 @@ namespace ConsoleApp1
             heightString = Console.ReadLine();
             height = double.Parse(heightString);
 
-            woodLength = 2 * (width + height) * 3.2;
+            woodLength = 2 * (width + height) * 3.25;
             glassArea = 2 * (width * height);
 
             Console.WriteLine("The length of the wood is " + woodLength + " feet");
---
+++ ConsoleApp1/notes.md
@@ -0,0 +1,13 @@
+﻿## Section 2 Simple Data Processing
+
+### Casting/Widening/Narrowing
+-The compiler will throw an error if you try to narrow a value:
+ex. int integerNumber = 1 -> float floatNumber = integerNumber
+
+-You have to explicity cast if you want this done: float floatNumber = (float)integerNumber
+
+-Be careful when casting values and look up the behavior if you're not sure what you end up getting.
+
+-You can do the reverse without casting:
+ex. double doubleNumber = 5.555555 -> decimal decimalNumber = doubleNumber
+
