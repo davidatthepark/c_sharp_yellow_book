47c460734e9d7e5ac61f82774dcb9163229abb09
davidatthepark <dpark4657@gmail.com>
2017-08-19 00:30:41 -0700

    finishes up to 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -12,22 +8,29 @@ namespace ConsoleApp1
         // You choose the names of your methods to reflect what they are going to do for you, except Main.
         static void Main(string[] args)
         {
-            // C# can handle three data types: floating point numbers, integer numbers, and text.
-            // A double is a double precision floating point number. Floats are simply numbers with decimal floats. From smallest to largest precision, we have floats(7), doubles(15), then decimal floats(29). Decimals use more space but they provide ultimate accuracy which is useful for things like currency. 
-            // Most programmers tend to use int and float variable types to make the programs easier to understand. Better not to fuss about the wasted memory. In ACOM, we deal with numbers that require accuracy, hence the use of decimals. 
-            // Choosing the right type of variable is a learned skill. Try to use floating point storage as a last resort. It lacks precision. Instead, you can represent 1.5 dollars as 150 cents.
+            const double MIN_WIDTH = 0.5;
+            const double MAX_WIDTH = 5.0;
+            const double MIN_HEIGHT = 0.75;
+            const double MAX_HEIGHT = 3.0;
 
             double width, height, woodLength, glassArea;
             string widthString, heightString;
 
-            Console.WriteLine("Welcome to Glazer Calculator. Please enter the width of the window.");
-            widthString = Console.ReadLine();
-            // Using a double here is not recommended because it does not take into account the precision required or how accurately the window can be measured. 
-            width = double.Parse(widthString);
+            do
+            {
+                Console.WriteLine("Give the width of the window between " + MIN_WIDTH + " and " + MAX_WIDTH + " :");
+                widthString = Console.ReadLine();
+                width = double.Parse(widthString);
+            }
+            while (width < MIN_WIDTH || width > MAX_WIDTH);
 
-            Console.WriteLine("Please enter the height of the window");
-            heightString = Console.ReadLine();
-            height = double.Parse(heightString);
+            do
+            {
+                Console.WriteLine("Give the height of the window between " + MIN_HEIGHT + " and " + MAX_HEIGHT + " :");
+                heightString = Console.ReadLine();
+                height= double.Parse(heightString);
+            }
+            while (height < MIN_HEIGHT || height > MAX_HEIGHT);
 
             woodLength = 2 * (width + height) * 3.25;
             glassArea = 2 * (width * height);
--- ConsoleApp1/notes.md
+++ ConsoleApp1/notes.md
@@ -1,13 +1,38 @@
-﻿## Section 2 Simple Data Processing
+﻿### Data Types
 
-### Casting/Widening/Narrowing
--The compiler will throw an error if you try to narrow a value:
+- C# can handle three data types: floating point numbers, integer numbers, and text.
+
+- A double is a double precision floating point number. Floats are simply numbers with decimal floats. From smallest to largest precision, we have floats(7), doubles(15), then decimal floats(29). Decimals use more space but they provide ultimate accuracy which is useful for things like currency. 
+
+- Most programmers tend to use int and float variable types to make the programs easier to understand. Better not to fuss about the wasted memory. In ACOM, we deal with numbers that require accuracy, hence the use of decimals. 
+
+- Choosing the right type of variable is a learned skill. Try to use floating point storage as a last resort. It lacks precision. Instead, you can represent 1.5 dollars as 150 cents.
+
+### Section 2 Simple Data Processing
+
+- All C# statements return a value.
+
+- Always strive for simplicity.
+
+##### Casting/Widening/Narrowing
+- The compiler will throw an error if you try to narrow a value:
 ex. int integerNumber = 1 -> float floatNumber = integerNumber
 
--You have to explicity cast if you want this done: float floatNumber = (float)integerNumber
+- You have to explicity cast if you want this done: float floatNumber = (float)integerNumber
 
--Be careful when casting values and look up the behavior if you're not sure what you end up getting.
+- Be careful when casting values and look up the behavior if you're not sure what you end up getting.
 
--You can do the reverse without casting:
+- You can do the reverse without casting:
 ex. double doubleNumber = 5.555555 -> decimal decimalNumber = doubleNumber
 
+##### Adjusting Real Number Precision
+
+- Placeholders can have formatting information added to them: 
+
+```
+int i = 150;
+double f = 1234.56789
+Console.WriteLine("i: {0:0} f: {1:0.00}, i, f");
+
+// i: 150 f: 1234.57
+```
