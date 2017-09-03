a367abc0767d2a2b7229973ea1f023dfc5f24c77
davidatthepark <dpark4657@gmail.com>
2017-09-03 14:28:43 -0700

    5.2.4 handling different accounts

--- ConsoleApp1/IAccount.cs
+++ ConsoleApp1/IAccount.cs
@@ -2,10 +2,7 @@
 {
     public interface IAccount
     {
-        bool SetName(string inName);
         string GetName();
-        bool SetAddress(string inAddress);
-        string GetAddress();
         void DepositFunds(decimal amount);
         bool WithdrawFunds(decimal amount);
         decimal GetBalance();
